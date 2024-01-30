29c60404a9e8eb48dafdb9866ba082943dba08f0
Oscar <10717328@ieselcaminas.org>
2024-01-30 16:42:57 +0100

    First commit

---
+++ src/main/java/org/uguinformatica/bluemoon_api/ApiRetoBluemoonApplication.java
@@ -0,0 +1,13 @@
+package org.uguinformatica.bluemoon_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRetoBluemoonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRetoBluemoonApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/uguinformatica/bluemoon_api/ApiRetoBluemoonApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uguinformatica.bluemoon_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRetoBluemoonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
