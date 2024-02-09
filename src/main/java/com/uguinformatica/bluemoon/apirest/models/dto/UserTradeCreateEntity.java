769d088aba6d325873021620d253012191a7c790
Oscar <10717328@ieselcaminas.org>
2024-02-09 17:26:14 +0100

    St-56: Create trades

--- src/main/java/com/uguinformatica/bluemoon/apirest/models/dto/UserTradeCreateEntity.java
+++ src/main/java/com/uguinformatica/bluemoon/apirest/models/dto/UserTradeCreateEntity.java
@@ -11,9 +11,6 @@ import java.util.List;
 @ToString
 public class UserTradeCreateEntity {
 
-    @Min(value = 1, message = "The id must be valid")
-    public long userId;
-
     @NotEmpty(message = "The trade must have at least one tradeable")
     @Valid
     public List<TradeableCreateEntity> tradeables;
